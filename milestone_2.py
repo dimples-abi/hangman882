afa99424cac56b2b4f6f21d35fd4c04d4f66ecf4
dimples-abi <abigail.hobden@gmail.com>
2023-11-20 20:01:40 +0000

    Made some changes to refactor the code

--- milestone_2.py
+++ milestone_2.py
@@ -1,7 +1,14 @@
 import random
-word_list = ['apple', 'pear', 'cherry', 'raspberry', 'clementine']
-word = random.choice(word_list)
-guess = input('Enter a single letter: ')
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
-else: print("Oops! That is not a valid input.")
+
+fruit_word_list = ['apple', 'pear', 'cherry', 'raspberry', 'clementine']
+
+random_word = random.choice(fruit_word_list)
+
+user_guess = input('Enter a single letter: ')
+if len(user_guess) == 1 and user_guess.isalpha():
+    if user_guess in random_word:
+        print("Good guess!")
+    else:
+        print("Oops! That letter is not in the word.")
+else:
+    print("Oops! That is not a valid input.")
